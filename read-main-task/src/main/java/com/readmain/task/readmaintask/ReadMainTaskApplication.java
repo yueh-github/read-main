4cdbad53c519ce2c121577972e7d4cfc00245077
yuehao <83541408@qq.com>
2017-09-12 16:39:17 +0800

    新增定时器消费者服务

---
+++ read-main-task/src/main/java/com/readmain/task/readmaintask/ReadMainTaskApplication.java
@@ -0,0 +1,12 @@
+package com.readmain.task.readmaintask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadMainTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadMainTaskApplication.class, args);
+	}
+}
---
+++ read-main-task/src/test/java/com/readmain/task/readmaintask/ReadMainTaskApplicationTests.java
@@ -0,0 +1,16 @@
+package com.readmain.task.readmaintask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReadMainTaskApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
